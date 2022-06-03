8e1b265416949eafd4894365d83b7efd9995d357
uki <ukiworks034@gmail.com>
2022-06-03 18:30:34 -0400

    demo user set up

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import SignUpModal from '../SignupFormPage/SignUpModal';
+import { demouser } from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
+  const dispatch = useDispatch();
+
+  const demoOnClick = async (e) => {
+    e.preventDefault();
+    await dispatch(demouser())
+  }
 
   return (
     (sessionUser ?
@@ -18,6 +25,10 @@ function Navigation({ isLoaded }) {
       <>
         <LoginFormModal />
         <SignUpModal />
+        <button
+          onClick={demoOnClick}
+        >Demo
+        </button>
       </>
     )
   )
